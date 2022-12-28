d883f7d478eb9230cfb4c49904879adb01de51c7
DonghunAn <iandonghun@gmail.com>
2022-12-28 20:09:03 +0900

    Refactor global CSS from .css to styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import Router from './shared/Router';
-import './App.css';
+import React from "react";
+import Router from "./shared/Router";
 
 function App() {
   return <Router />;
--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ import App from "./App";
 import store from "./redux/config/configStore";
 import { Provider } from "react-redux";
 
+import GlobalStyle from "./components/styles/GlobalStyle";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //App을 Provider로 감싸주고, configStore에서 export default 한 store를 넣어줍니다.
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>
 );
